54ae0b392167aeb01d712618c9b47972b4a23c9e
Gonzalez32 <3232rose@gmail.com>
2020-12-28 00:16:17 -0600

    modified index.js

--- my-react-app/src/components/counter.jsx
+++ my-react-app/src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
     state = {
-        count: 0,
+        value: this.props.value,
         imageUrl: 'https://s.clipartkey.com/mpngs/s/24-248983_transparent-pitbull-clipart-png-straight-line-drawing-dog.png',
         tags: []
     };
@@ -13,11 +13,12 @@ class Counter extends Component {
     // }
 
     handleIncrement = product => {
-        console.log(product);
-        this.setState({ count: this.state.count + 1 })
+        // console.log(product);
+        this.setState({ value: this.state.value + 1 })
     }
 
   render() {
+
     return (
       <React.Fragment>
             <img src={this.state.imageUrl} alt=""/>
@@ -40,12 +41,12 @@ class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
+        const { value: count } = this.state;
         return count === 0 ? 'Zero' : count;
     }
 }
