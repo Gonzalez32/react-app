e34f90f6b1ed4db3205ab1cc042da60f68d94181
Gonzalez32 <3232rose@gmail.com>
2021-01-02 21:27:34 -0600

    modified navbar.jsx

---
+++ my-react-app/src/components/navbar.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { Component } from "react";
+
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
